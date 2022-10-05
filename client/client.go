af686af64e6dbf1fa7b05e45bba8486276b4026e
oljh <oljh@itu.dk>
2022-10-05 16:09:34 +0200

    New Server

--- client/client.go
+++ client/client.go
@@ -1,14 +1,44 @@
-package client
+package main
 
+import (
+	"context"
+	"flag"
+	"fmt"
+	"time"
 
+	"github.com/OliFryser/Exercises5/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+// Same principle as in client. Flags allows for user specific arguments/values
+var clientsName = flag.String("name", "default", "Senders name")
+var serverPort = flag.String("server", "5400", "Tcp server")
+
+var server proto.TimeServiceClient //the server
+var ServerConn *grpc.ClientConn    //the server connection
 
 func main() {
 	var opts []grpc.DialOption
 	opts = append(
-		opts, grpc.WithBlock(), 
-		grpc.WithTransportCredentials(insecure.NewCredentials())
+		opts, grpc.WithBlock(),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
 	)
 
 	timeContext, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
+
+	conn, _ := grpc.DialContext(
+		timeContext,
+		fmt.Sprintf(":%s", *serverPort),
+		opts...,
+	)
+
+	server = proto.NewTimeServiceClient(conn)
+
+	treq := proto.TimeRequest{
+		T1: "Current Time",
+	}
+	server.GetServerTime(timeContext, &treq)
+
 }
--- server/server.go
+++ server/server.go
@@ -1,32 +1,35 @@
-package server
+package main
 
 import (
 	"context"
+	"net"
 
+	"github.com/OliFryser/Exercises5/proto"
 	"google.golang.org/grpc"
 )
 
 type Server struct {
 	// an interface that the server needs to have
-	gRPC.UnimplementedTemplateServer
+	proto.UnimplementedTimeServiceServer
 
 	// here you can impliment other fields that you want
 }
 
 func main() {
+	list, _ := net.Listen("tcp", ":9080")
+
+	var opts []grpc.ServerOption
 	grpcServer := grpc.NewServer(opts...)
-	server := &Server
-	gRPC.RegisterTemplateServer(grpcServer, server)
+	server := &Server{}
+	proto.RegisterTimeServiceServer(grpcServer, server)
 	grpcServer.Serve(list)
 }
 
-func (s *Server) GetServerTime(ctx context.Context, t1 *TimeRequest) (*TimeResponse, error) {
+func (s *Server) GetServerTime(ctx context.Context, t1 *proto.TimeRequest) (*proto.TimeResponse, error) {
 	t2 := "time.Now()"
-	tr := TimeResponse{
-		t2: "bob",
-		t3: "lop",
+	tr := proto.TimeResponse{
+		T2: t2,
+		T3: "lop",
 	}
-	timeResponse := new(TimeResponse())
-
-	//return (tr, nil)
+	return &tr, nil
 }
